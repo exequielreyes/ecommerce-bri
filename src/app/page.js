d29a9ee474901018b892ce2e84144cfd330d12d5
exequielreyes <exequielreyes93@gmail.com>
2024-07-10 21:27:08 -0300

    muestro 6 productos en la pagina de inicio y aplique un diseño para que sea responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,24 @@
 import PostCard from "./components/PostCard";
 
-
 //de esta forma mostramos los producto en la pagina de inicio
-async function fetchProduct(){
-    const res = await fetch("https://fakestoreapi.com/products");
-    const data = await res.json();
-    return data
+async function fetchProduct() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return data.slice(0, 6);
 }
 
+export default async function Home() {
+  const products = await fetchProduct();
 
-export default async  function Home() {
-    const products = await fetchProduct()
-return(
-    <div>
-        {products && products.map((product) =>(
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold text-center my-8">Mas populares</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+        {products &&
+          products.map((product) => (
             <PostCard key={product.id} product={product} />
-        ))}
-
+          ))}
+      </div>
     </div>
-)
+  );
 }
